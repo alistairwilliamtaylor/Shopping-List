394a36bd2b9a22d599289cc1e56e25497fa483fd
hydenseoMYOB <haedeun.seo@myob.com>
2022-06-01 13:07:04 +1000

    shopping list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [items, setItems] = useState([
+        {name: "Milk", crossed: false}
+    ]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleAddButton = () => {
+        const newItem = {
+            name: inputValue,
+            crossed: false,
+        };
+
+        const newItems = [...items, newItem];
+
+        setItems(newItems);
+        setInputValue('');
+    }
+
+    const crossOut = (indexOfItemToCrossOut: any) => {
+        const newItems = items.map((item,index) => {
+            return index == indexOfItemToCrossOut ? crossoutItem(item) : item;
+        })
+        setItems(newItems);
+    }
+
+    const crossoutItem = (oldItem : any) => {
+            const newItem = {
+            name: oldItem.name,
+            crossed: true,
+            };
+        return newItem;
+    }
+    const handleSubmit = (event : any) => {
+        event.preventDefault();
+        return false;
+    }
+
+    return (
+        <body>
+        <h1>My Shopping List</h1>
+          <form className="item-adder" onSubmit={(e) => handleSubmit(event)}>
+            <label>What would you like to add?</label>
+            <input className="user-input" type="text" onChange={(event) => setInputValue(event.target.value)}/>
+              <button className="add-button" onClick={() => handleAddButton()}>Add me!</button>
+          </form>
+        <section className="common-items">
+            {items.map((item, index) => (
+                <div className ="box">
+                    <label className = {item.crossed ? "StrikeThrough" : "label"}>
+                        {item.name}
+                    </label>
+                    <button disabled={item.crossed} className="tick" onClick={() => crossOut(index)}>Tick!</button>
+                </div>
+                ))}
+        </section>
+        </body>
+    );
 }
 
 export default App;
